25afd3206d9d36e1d2a07f1e73992ed1777bfd90
RouSage <kolbasovsergey02@gmail.com>
2023-01-07 15:53:46 +0100

    feat: connect wallet logic

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -2,12 +2,16 @@ import useSWR from "swr";
 
 import { CryptoHookFactory } from "types/hooks";
 
-type AccountHookFactory = CryptoHookFactory<string>;
+type UseAccountResponse = {
+  connect: () => Promise<void>;
+};
+
+type AccountHookFactory = CryptoHookFactory<string, UseAccountResponse>;
 
 export type UseAccountHook = ReturnType<AccountHookFactory>;
 
 export const hookFactory: AccountHookFactory =
-  ({ provider }) =>
+  ({ ethereum, provider }) =>
   (params) => {
     const swrResponse = useSWR(
       provider ? "web3/useAccount" : null,
@@ -24,5 +28,13 @@ export const hookFactory: AccountHookFactory =
       { revalidateOnFocus: false }
     );
 
-    return swrResponse;
+    const connect = async () => {
+      try {
+        ethereum?.request({ method: "eth_requestAccounts" });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    return { ...swrResponse, connect };
   };
--- types/hooks.ts
+++ types/hooks.ts
@@ -8,9 +8,9 @@ export type Web3Dependencies = {
   provider: providers.Web3Provider;
 };
 
-export type CryptoHookFactory<D = any, P = any> = {
-  (d: Partial<Web3Dependencies>): CryptoHandlerHook<D, P>;
+export type CryptoHookFactory<D = any, R = any, P = any> = {
+  (d: Partial<Web3Dependencies>): CryptoHandlerHook<D, R, P>;
 };
 
-type CryptoHandlerHook<D, P> = (params?: P) => CryptoSWRResponse<D>;
-type CryptoSWRResponse<D> = SWRResponse<D>;
+type CryptoHandlerHook<D, R, P> = (params?: P) => CryptoSWRResponse<D, R>;
+type CryptoSWRResponse<D, R> = SWRResponse<D> & R;
