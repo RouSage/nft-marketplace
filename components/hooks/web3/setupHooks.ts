1635da49c9b74c7496049c6d0d0c609af2604a4e
RouSage <kolbasovsergey02@gmail.com>
2023-03-05 16:44:30 +0100

    feat: useListedNfts hook

--- components/hooks/web3/setupHooks.ts
+++ components/hooks/web3/setupHooks.ts
@@ -1,14 +1,20 @@
 import { Web3Dependencies } from "types/hooks";
 
 import { hookFactory as createAccountHook, UseAccountHook } from "./useAccount";
+import {
+  hookFactory as createListedNftsHook,
+  UseListedNftsHook,
+} from "./useListedNfts";
 import { hookFactory as createNetworkHook, UseNetworkHook } from "./useNetwork";
 
 export type Web3Hooks = {
   useAccount: UseAccountHook;
   useNetwork: UseNetworkHook;
+  useListedNfts: UseListedNftsHook;
 };
 
 export const setupHooks = (d: Web3Dependencies): Web3Hooks => ({
   useAccount: createAccountHook(d),
   useNetwork: createNetworkHook(d),
+  useListedNfts: createListedNftsHook(d),
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
+import { useListedNfts } from "components/hooks/web3";
 import { BaseLayout, NftList } from "components/ui";
-import nfts from "content/meta.json";
 import { NftMeta } from "types/nft";
 
 const Home = () => {
+  const { nfts } = useListedNfts();
+
+  console.log(nfts.data);
+
   return (
     <BaseLayout>
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
@@ -18,7 +22,7 @@ const Home = () => {
               Mint a NFT to get unlimited ownership forever!
             </p>
           </section>
-          <NftList nfts={nfts as NftMeta[]} />
+          <NftList nfts={nfts.data} />
         </div>
       </div>
     </BaseLayout>
